c5bd231f6c65aeba68a2cf6eb813304ab289680a
JamesHagerty1 <70479852+JamesHagerty1@users.noreply.github.com>
2024-12-03 20:14:06 -0500

    app to demo formschema lib

--- formschema_demo/src/App.vue
+++ formschema_demo/src/App.vue
@@ -1,7 +1,13 @@
+<!--
+Demo formschema/native Vue lib:
+https://www.npmjs.com/package/@formschema/native
+-->
+
+
 <script setup>
     // TODO figure out how to user vue-router
     // import { RouterLink, RouterView } from 'vue-router'
-
+    import FormSchema from '@formschema/native'
 </script>
 
 
